cf7d94a8ce32faec4dfb33c8a068d4b6e5c0c4c4
Beknin <iposohov1@gmail.com>
2026-07-03 17:16:41 +0800

    modified admin window to use the new topics tab

--- client/gui/admin_window.py
+++ client/gui/admin_window.py
@@ -2,6 +2,8 @@ from tkinter import ttk
 from gui.app import App
 from gui.widgets.status_bar import StatusBar
 from gui.frames.admin.users_frame import UsersFrame
+from gui.frames.admin.topics_frame import TopicsFrame
+
 
 class AdminWindow(App):
     def __init__(self, user: dict, api_client):
@@ -23,10 +25,12 @@ class AdminWindow(App):
         self.users_frame = UsersFrame(notebook, self.api)
         notebook.add(self.users_frame, text="Пользователи")
 
-        for tab_name in ["Темы", "Назначения", "Отчёты"]:
+        self.topics_frame = TopicsFrame(notebook, self.api)
+        notebook.add(self.topics_frame, text="Темы")
+
+        for tab_name in ["Назначения", "Отчёты"]:
             frame = ttk.Frame(notebook)
-            ttk.Label(frame, text=f"Вкладка: {tab_name}",
-                      font=("Arial", 16)).pack(expand=True)
+            ttk.Label(frame, text=f"Вкладка: {tab_name}").pack(expand=True)
             notebook.add(frame, text=tab_name)
 
         self.status_bar = StatusBar(self.window)
